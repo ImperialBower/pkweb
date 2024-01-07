83416e4a2602f2cc4d96dd40f767f7d87a791e0d
folkengine <gaoler@electronicpanopticon.com>
2024-01-07 11:28:50 -0800

    Added plus stripper and tests

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,55 @@
+use std::borrow::Cow;
 use warp::Filter;
 
-/// http://127.0.0.1:3030/hello/A%E2%99%A0+J%E2%99%A6+6%E2%99%A5+6%E2%99%A3
+///
 #[tokio::main]
 async fn main() {
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    let hello =
+        warp::path!("hello" / String).map(|name| format!("Hello, {}!", decode_string(name)));
 
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+}
+
+fn decode(s: &str) -> String {
+    replace_plus(
+        percent_encoding::percent_decode_str(s)
+            .decode_utf8()
+            .unwrap(),
+    )
+}
+
+fn decode_string(s: String) -> String {
+    decode(s.as_str())
+}
+
+/// I will confess that the magic that this method can take a char
+/// and a Cow<str> and return a String is beyond me (hopefully, at
+/// the moment).
+fn replace_plus(s: Cow<str>) -> String {
+    s.replace('+', " ")
+}
+
+#[cfg(test)]
+#[allow(non_snake_case)]
+mod main_tests {
+    #[test]
+    fn decode() {
+        let s = "A%E2%99%A0+J%E2%99%A6+6%E2%99%A5+6%E2%99%A3";
+
+        assert_eq!(super::decode(s), "A♠ J♦ 6♥ 6♣");
+    }
+
+    #[test]
+    fn decode_string() {
+        let s = "A%E2%99%A0+J%E2%99%A6+6%E2%99%A5+6%E2%99%A3";
+
+        assert_eq!(super::decode_string(s.into()), "A♠ J♦ 6♥ 6♣");
+    }
+
+    #[test]
+    fn replace_plus() {
+        let s = "A♠+J♦+6♥+6♣";
+
+        assert_eq!(super::replace_plus(s.into()), "A♠ J♦ 6♥ 6♣");
+    }
 }
